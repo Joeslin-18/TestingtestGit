8f3ebe70e4067cd558a0ea5a55740d26a46bff84
NITHIYA S <nithyajoeslin@gmail.com>
2025-01-30 19:52:04 +0530

    Have created new class and pushed to main

--- src/test/java/Pushingfile.java
+++ src/test/java/Pushingfile.java
@@ -1,9 +1,9 @@
 public class Pushingfile {
-    public static void main(String[] args) {
-        System.out.println("Updating 1111file-1");
-    }
+public static void main(String[] args) {
 
 
+  System.out.println("_new class");
+}
 }
 
 
